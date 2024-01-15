4835c173a92cbf73d23c5b6b437bba8b28531263
Mario-Torralba <123827065+Mario-Torralba@users.noreply.github.com>
2024-01-15 12:53:22 +0100

    .

--- Interfaces/di23/di23/vistas/V_Menu.php
+++ Interfaces/di23/di23/vistas/V_Menu.php
@@ -1,19 +1,10 @@
 <?php
 
-    echo "<nav class='navbar navbar-expand-sm navbar-light' style='background-color: #68a8f1' aria-label='Fourth navbar example'>
-        <div class='container-fluid'>
-            <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarsExample04' aria-controls='navbarsExample04' aria-expanded='false' aria-label='Toggle navigation'>
-                <span class='navbar-toggler-icon'></span>
-            </button>
-
-            <div class='collapse navbar-collapse' id='navbarsExample04'>
-                <ul class='navbar-nav me-auto mb-2 mb-md-0'>";
-            
     $menu = $datos['menu'];
 
     $arrayPadres = array();
-    $contador = 1;
     $arrayHijos = array();
+    $contador = 1;
 
     foreach ($menu as $dato) {
 
@@ -34,35 +25,34 @@
         }
 
         if($dato['ID_PADRE'] != 0){
-            $arrayPadres[$dato['ID_PADRE']][0] = $dato;
+            array_push($arrayPadres[$dato['ID_PADRE']][1],$dato);
             $contadorHijo++;
             $idDeMismoPadreParaReseteoDelContadorDeHijos = $dato['ID_PADRE'];
         }
+
     }
 
-    foreach ($arrayPadres as $elemento) {
+    print_r($arrayPadres);
 
-        echo $elemento[0]['ID_PADRE'];
-        // foreach ($elemento[1] as $elemento2) {
-        //     echo $elemento2[0]['ID_PADRE'];
-        // }
-    }
+    echo "<nav class='navbar navbar-expand-sm navbar-light' style='background-color: #68a8f1' aria-label='Fourth navbar example'>
+        <div class='container-fluid'>
+            <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarsExample04' aria-controls='navbarsExample04' aria-expanded='false' aria-label='Toggle navigation'>
+                <span class='navbar-toggler-icon'></span>
+            </button>
 
-    foreach($menu as $fila){
+            <div class='collapse navbar-collapse' id='navbarsExample04'>
+                <ul class='navbar-nav me-auto mb-2 mb-md-0'>";
+            
+    
 
-        if ($fila["ID_PADRE"] == 0) {
-
-            if($fila["ORDEN"] == 1){
-                
-            }
-            echo    "<li class='nav-item'>
-                        <a class='nav-link' href='#' style='color: white' onclick='".$fila['ACCION']."'>".$fila['TITULO']."</a>
-                    </li>";
+    foreach ($arrayPadres as $e) {
+        foreach($arrayPadres[$e] as $e2){
+            
         }
-
     }
 
-//     "           <li class='nav-item'>
+    
+//                "<li class='nav-item'>
 //                     <a class='nav-link active' style='color: white' aria-current='page' href='#'>Home</a>
 //                 </li>
 //                 <li class='nav-item'>
@@ -88,19 +78,6 @@ echo        "</ul>
     </div>
 </nav>";
 
-$a = array('1','2','3',array(3.1,3.2,3.3));
-
-
-foreach ($a as $b) {
-    if(gettype($b) == 'string'){
-        echo $b;
-    }else{
-        foreach ($b as $c) {
-            echo $c;
-        }
-    }
-    
-}
 
 
 
