4e9c7d389892da9f3ddaeffc08ec607d22cd4f77
Mario-Torralba <123827065+Mario-Torralba@users.noreply.github.com>
2024-02-20 12:55:15 +0100

    .

--- Interfaces/di23/di23/modelos/M_Seguridad.php
+++ Interfaces/di23/di23/modelos/M_Seguridad.php
@@ -24,32 +24,61 @@
 
             $nombre_texto = '';
             $rol_texto = '';
-
+            
             extract($filtros);
 
-            $SQL = 'SELECT
-            PU.ID_PERMISO_USUARIO,
-            P.ID_PERMISO,
-            P.NOMBRE_PERMISO,
-            RU.ID_ROL_USUARIO,
-            R.ID_ROL,
-            R.NOMBRE_ROL,
-            M.ID_MENU,
-            M.TITULO,
-            M.ID_PADRE,
-            M.ACCION,
-            M.ORDEN,
-            M.PRIVADO
-        FROM USUARIO U
-        INNER JOIN PERMISO_USUARIO PU ON U.ID_USUARIO = PU.ID_USUARIO
-        INNER JOIN PERMISO P ON PU.ID_PERMISO = P.ID_PERMISO
-        INNER JOIN ROL_USUARIO RU ON U.ID_USUARIO = RU.ID_USUARIO
-        INNER JOIN ROL R ON RU.ID_ROL = R.ID_ROL
-        INNER JOIN MENU M ON P.ID_MENU = M.ID_MENU
-        WHERE U.ID_USUARIO = '.$nombre_texto.'
-        ORDER BY M.ID_PADRE ASC, M.ORDEN ASC;';
+            if($nombre_texto!=''){
+                $SQL = 'SELECT
+                U.ID_USUARIO
+                PU.ID_PERMISO_USUARIO,
+                P.ID_PERMISO,
+                P.NOMBRE_PERMISO,
+                RU.ID_ROL_USUARIO,
+                R.ID_ROL,
+                R.NOMBRE_ROL,
+                M.ID_MENU,
+                M.TITULO,
+                M.ID_PADRE,
+                M.ACCION,
+                M.ORDEN,
+                M.PRIVADO
+                FROM USUARIO U
+                INNER JOIN PERMISO_USUARIO PU ON U.ID_USUARIO = PU.ID_USUARIO
+                INNER JOIN PERMISO P ON PU.ID_PERMISO = P.ID_PERMISO
+                INNER JOIN ROL_USUARIO RU ON U.ID_USUARIO = RU.ID_USUARIO
+                INNER JOIN ROL R ON RU.ID_ROL = R.ID_ROL
+                INNER JOIN MENU M ON P.ID_MENU = M.ID_MENU
+                WHERE U.ID_USUARIO = '.$nombre_texto.'
+                ORDER BY M.ID_PADRE ASC, M.ORDEN ASC;';
+            }else{
+
+                $SQL = 'SELECT
+                U.ID_USUARIO,
+                PU.ID_PERMISO_USUARIO,
+                P.ID_PERMISO,
+                P.NOMBRE_PERMISO,
+                RU.ID_ROL_USUARIO,
+                R.ID_ROL,
+                R.NOMBRE_ROL,
+                M.ID_MENU,
+                M.TITULO,
+                M.ID_PADRE,
+                M.ACCION,
+                M.ORDEN,
+                M.PRIVADO
+                FROM USUARIO U
+                INNER JOIN PERMISO_USUARIO PU ON U.ID_USUARIO = PU.ID_USUARIO
+                INNER JOIN PERMISO P ON PU.ID_PERMISO = P.ID_PERMISO
+                INNER JOIN ROL_USUARIO RU ON U.ID_USUARIO = RU.ID_USUARIO
+                INNER JOIN ROL R ON RU.ID_ROL = R.ID_ROL
+                INNER JOIN MENU M ON P.ID_MENU = M.ID_MENU
+                ORDER BY M.ID_PADRE ASC, M.ORDEN ASC;';
+            }
+
+            echo $SQL;
             $menus=$this->DAO->consultar($SQL);
 
+    
             return $menus;
 
         }
--- Interfaces/di23/di23/vistas/V_SeguridadBusquedaMenus.php
+++ Interfaces/di23/di23/vistas/V_SeguridadBusquedaMenus.php
@@ -1,3 +1,28 @@
 <?php
-    print_r($datos['menus']);
+    $datos = $datos['menus'];
+    $usuarios = array();
+    json_encode($datos);
+
+    foreach ($datos as $element) {
+        $salida = false;
+        foreach ($usuarios as $usuario) {
+            if($usuario == $element['ID_USUARIO']){
+                $salida = true;
+                break;
+            }else{
+                $salida = false;
+            }
+        }
+        if(!$salida){
+            array_push($usuarios, $element['ID_USUARIO']);
+        }
+    }
+
+    sort($usuarios);
+
+    foreach ($usuarios as $usuario) {
+        ?>
+        <div class="contenedor_usuario"></div>
+        <?php
+    }
 ?>
